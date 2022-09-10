39182bc1ac604853546653768c3662b03c95c144
thaxz <thais.monteiroc@ufpe.br>
2022-09-10 15:18:57 -0300

    finishing basic constraints

---
+++ ChallengeParis/RatingsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RatingsViewController.swift
+//  ChallengeParis
+//
+//  Created by thaxz on 10/09/22.
+//
+
+import UIKit
+
+class RatingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
